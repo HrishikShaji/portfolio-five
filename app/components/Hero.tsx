2b983a63abf68c9c27a57a26f3a2c71880e1e0e1
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-04 15:23:24 +0530

    added layout

---
+++ app/components/Hero.tsx
@@ -0,0 +1,61 @@
+export const Hero = () => {
+  return (
+    <div className="text-white  grid grid-cols-4 grid-rows-10 gap-4">
+      <div className="p-10 bg-black rounded-3xl ">
+        <h1 className="text-8xl">My Name is,</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl col-span-3">
+        <h1 className="text-8xl whitespace-nowrap">Hello There,</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl col-span-3">
+        <h1 className="text-9xl">Anakin Skywalker.</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl  row-span-3">
+        <h1 className="text-8xl">About</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl row-span-3">
+        <h1 className="text-9xl">spacer about</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl row-span-2 col-span-2">
+        <h1 className="text-9xl">Anakin Skywalker.</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl ">
+        <h1 className="text-9xl">naoo</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl ">
+        <h1 className="text-9xl">naoo</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl ">
+        <h1 className="text-9xl">naoo</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl col-span-2 ">
+        <h1 className="text-9xl">Skills</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl col-span-2  row-span-2">
+        <h1 className="text-9xl">Skills</h1>
+      </div>
+
+      <div className="p-10 bg-black rounded-3xl   row-span-2">
+        <h1 className="text-9xl">testimonials</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl   ">
+        <h1 className="text-9xl">fake</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl  col-span-2 ">
+        <h1 className="text-9xl">testi</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl  row-span-3 ">
+        <h1 className="text-9xl">see preojects</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl  row-span-2 col-span-3">
+        <h1 className="text-9xl"> preojects</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl   ">
+        <h1 className="text-9xl">contact me</h1>
+      </div>
+      <div className="p-10 bg-black rounded-3xl  col-span-3">
+        <h1 className="text-9xl"> contact</h1>
+      </div>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Hero } from "./components/Hero";
+
 export default function Home() {
-	return <main className=""></main>;
+  return (
+    <main className="bg-red-500 p-5 w-full h-full">
+      <Hero />
+    </main>
+  );
 }
