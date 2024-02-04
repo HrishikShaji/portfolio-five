e3796f5782cdb3978d2f51ade2ebb361b7e3e2b5
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-04 15:56:06 +0530

    color coded

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { Hero } from "./components/Hero";
 
 export default function Home() {
-  return (
-    <main className="bg-red-500 p-5 w-full h-full">
-      <Hero />
-    </main>
-  );
+	return (
+		<main className="bg-neutral-700 p-5 w-full h-full">
+			<Hero />
+		</main>
+	);
 }
